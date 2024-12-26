739a65aa3934abf58d67f333e808a2c965ecd92c
Aditya <aadi099012@gmail.com>
2024-12-26 14:33:46 +0530

    Db connection

---
+++ db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../src/constants.js";
+
+const DB_CONNECT = async()=>{
+    try {
+       const ConnectionInstances = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       console.log(`\n MONGODB CONNECTED : HOST-> ${ConnectionInstances.connection.host} `)
+    } catch (error) {
+        console.error(`MongoDB connection error !!`,error);
+        process.exit(1);
+        
+    }
+}
+
+export default DB_CONNECT;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import dotenv from "dotenv"
+import DB_CONNECT from "../db/index.js"
+
+dotenv.config({
+    path:'./env'
+})
+
+DB_CONNECT();
